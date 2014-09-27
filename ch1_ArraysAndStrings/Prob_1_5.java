bad87826c06121d526d6a3c997dfaff53bcd6aa0
akshaymatoo <akshaymattoo@hotmail.com>
2014-09-27 17:24:21 -0500

    Update Prob_1_5.java

--- ch1_ArraysAndStrings/Prob_1_5.java
+++ ch1_ArraysAndStrings/Prob_1_5.java
@@ -6,3 +6,22 @@ Java has built in replace functionality
 use String > .replace(replaceWhat, replaceWith)
 */
 
+public class StringReplace {
+
+	public static void main(String[] args) {
+		String str="Hi  i  am  a  boy";
+		
+		
+		StringBuilder sb =  new StringBuilder();
+		
+		for (int i = 0; i < str.length(); i++) {
+			int character = (int)str.charAt(i); 
+			if(character == 32)
+				sb.append("%20");
+			else
+				sb.append(str.charAt(i));
+			
+		}
+		System.out.println(sb.toString());
+		
+	}
